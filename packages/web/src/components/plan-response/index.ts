bcbb2a98eff19209d5ddfa380ccc5995cd930dee
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2026-03-26 21:44:26 +0530

    chore: add barrel exports for new components

--- packages/web/src/components/plan-response/index.ts
+++ packages/web/src/components/plan-response/index.ts
@@ -2,3 +2,6 @@ export { PlanResponse } from './plan-response.js';
 export { MetricsBar } from './metrics-bar.js';
 export { ActionsFooter } from './actions-footer.js';
 export { MarkdownRenderer } from './markdown-renderer.js';
+export * from './primitives/index.js';
+export * from './cards/index.js';
+export * from './charts/index.js';
